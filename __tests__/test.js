92b637ebebc3017c9d25231c6a7d5637814f6966
Saitgalin Aleksandr <cred1tor@yandex.ru>
2020-05-09 21:17:39 +0600

    first working build. implemented the load function

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,5 +1,5 @@
 import path from 'path';
-import fs from 'fs';
+import { promises as fs } from 'fs';
 import nock from 'nock';
 import os from 'os';
 import _ from 'lodash';
@@ -9,17 +9,17 @@ const getFixturePath = (fixtureName) => path.join(__dirname, `__fixtures__/${fix
 const readFixture = (fixtureName) => fs.readFile(getFixturePath(fixtureName), 'utf-8');
 
 beforeEach(async () => {
-  await fs.unlink(path.join(os.tmpdir(), 'fakeadress-com.html')).catch(_.noop);
+  await fs.unlink(path.join(os.tmpdir(), 'fakeaddress-com.html')).catch(_.noop);
 });
 
 test('load and save a page', async () => {
   const data = await readFixture('page.html');
-  nock('https://fakeadress.com')
+  nock('https://fakeaddress.com')
     .log(console.log)
     .get('/')
     .reply(200, data);
 
   await load('https://fakeaddress.com', os.tmpdir());
-  const result = await fs.readFile(path.join(os.tmpdir(), 'fakeadress-com.html'));
+  const result = await fs.readFile(path.join(os.tmpdir(), 'fakeaddress-com.html'), 'utf-8');
   expect(result).toBe(data);
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import half from './half.js';
+import axios from 'axios';
+import path from 'path';
+import { promises as fs } from 'fs';
 
-export default half;
+const convertUrlToFileName = (sourceUrl) => {
+  const urlData = new URL(sourceUrl);
+  const urlWithoutOrigin = `${urlData.host}${urlData.pathname}`;
+  const fileName = urlWithoutOrigin.replace(/[^a-zA-Z0-9]/g, '-');
+  return fileName.slice(0, -1).concat('.html');
+};
+
+export default (sourceUrl, destDir = process.cwd()) => axios.get(sourceUrl)
+  .then((response) => {
+    const fileName = convertUrlToFileName(sourceUrl);
+    const dest = path.join(destDir, fileName);
+    return fs.writeFile(dest, response.data);
+  });
