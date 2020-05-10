126f950d84209662f6da00bf87bffa61bd749700
Saitgalin Aleksandr <cred1tor@yandex.ru>
2020-05-10 15:04:46 +0600

    removed async/await

--- src/commander.js
+++ src/commander.js
@@ -6,13 +6,10 @@ program.version(version)
   .description('Downloads a web page and saves it as html file.')
   .option('--output <dir>', 'output directory', process.cwd())
   .arguments('<srcUrl>')
-  .action(async (srcUrl) => {
-    try {
-      await load(srcUrl, program.output);
-    } catch (e) {
+  .action((srcUrl) => load(srcUrl, program.output)
+    .catch((e) => {
       console.log(e.message);
       process.exitCode = 1;
-    }
-  });
+    }));
 
 export default () => program.parse(process.argv);
