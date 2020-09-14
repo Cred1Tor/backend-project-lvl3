528dc9ad1d5f5dc2947b39e5ea410ced75c8c2bd
Saitgalin Aleksandr <cred1tor@yandex.ru>
2020-09-15 04:36:06 +0600

    added assets files saving

--- __tests__/test.js
+++ __tests__/test.js
@@ -10,16 +10,25 @@ const readFixture = (fixtureName) => fs.readFile(getFixturePath(fixtureName), 'u
 
 beforeEach(async () => {
   await fs.unlink(path.join(os.tmpdir(), 'fakeaddress-com.html')).catch(_.noop);
+  await fs.rmdir(path.join(os.tmpdir(), 'fakeaddress-com_files'), { recursive: true }).catch(_.noop);
 });
 
 test('load and save a page', async () => {
   const expectedHtml = await readFixture('page.html');
+  const expectedCss = await readFixture('123.css');
   nock('https://fakeaddress.com')
     .log(console.log)
     .get('/')
     .reply(200, expectedHtml);
 
-  await load('https://fakeaddress.com', os.tmpdir());
+  nock('https://fakeaddress.com')
+    .log(console.log)
+    .get('/123.css')
+    .reply(200, expectedCss);
+
+  await load('https://fakeaddress.com/', os.tmpdir());
   const result = await fs.readFile(path.join(os.tmpdir(), 'fakeaddress-com.html'), 'utf-8');
+  const resultCss = await fs.readFile(path.join(os.tmpdir(), 'fakeaddress-com_files/123.css'), 'utf-8');
   expect(result).toBe(expectedHtml);
+  expect(resultCss).toBe(expectedCss);
 });
--- src/index.js
+++ src/index.js
@@ -1,19 +1,62 @@
 import axios from 'axios';
 import path from 'path';
 import cheerio from 'cheerio';
+import url from 'url';
 import { promises as fs } from 'fs';
 
 const convertUrlToFileName = (sourceUrl) => {
   const urlData = new URL(sourceUrl);
   const urlWithoutOrigin = `${urlData.host}${urlData.pathname}`;
-  const fileName = urlWithoutOrigin.replace(/[^a-zA-Z0-9]/g, '-');
-  return fileName.slice(0, -1).concat('.html');
+  const fileName = urlWithoutOrigin.replace(/[^a-zA-Z0-9]/g, '-').replace(/-$/, '');
+  return fileName;
 };
 
-export default (sourceUrl, destDir = process.cwd()) => axios.get(sourceUrl)
-  .then((response) => {
-    const $ = cheerio.load(response.data);
-    const fileName = convertUrlToFileName(sourceUrl);
-    const dest = path.join(destDir, fileName);
-    fs.writeFile(dest, $.html());
-  });
+const convertRelUrlToFileName = (relUrl) => {
+  const pathData = path.parse(relUrl);
+  const fileName = `${pathData.dir.slice(1)}-${pathData.name}`.replace(/^-/, '').replace(/[^a-zA-Z0-9]/g, '-');
+  return `${fileName}${pathData.ext}`;
+};
+
+const saveFile = (source, dest) => axios.get(source)
+  .then((response) => fs.writeFile(dest, response.data));
+
+export default (sourceUrl, destDir = process.cwd()) => {
+  const sourceUrlData = new URL(sourceUrl);
+  const sourceHost = sourceUrlData.host;
+  const dirName = `${convertUrlToFileName(sourceUrl)}_files`;
+  const fileName = convertUrlToFileName(sourceUrl);
+  const dest = path.join(destDir, `${fileName}.html`);
+  const destAssetsDir = path.join(destDir, dirName);
+  let $;
+  let elements;
+  return axios.get(sourceUrl)
+    .then((response) => {
+      $ = cheerio.load(response.data);
+      return fs.writeFile(dest, $.html());
+    }).then(() => {
+      elements = $('link, script, img[src]');
+      if (elements.length === 0) {
+        throw new Error('no links');
+      }
+      return fs.mkdir(destAssetsDir).catch(() => {});
+    }).then(() => {
+      const promises = [];
+      elements.each((i, el) => {
+        const elSrc = cheerio(el).attr('src');
+        const elSrcUrl = new URL(elSrc, sourceUrl);
+        if (elSrcUrl.host !== sourceHost) {
+          return;
+        }
+        // console.log(elSrc);
+        const elFileName = convertRelUrlToFileName(elSrc);
+        const newPath = path.join(dirName, elFileName);
+        // console.log(elFileName);
+        // console.log(newPath);
+        $(el).attr('src', newPath);
+        promises.push(saveFile(url.resolve(sourceUrl, elSrc), path.join(destAssetsDir, elFileName)));
+      });
+      return Promise.all(promises);
+    });
+};
+
+// console.log(new URL('example.html', 'https://fake.com/123.html'));
