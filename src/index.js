ce2c7c4c884dabbc914e279d8fc45b1dbe25a80a
Saitgalin Aleksandr <cred1tor@yandex.ru>
2020-09-23 01:50:50 +0600

    added debug logging

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 import path from 'path';
 import cheerio from 'cheerio';
-import url from 'url';
-import _ from 'lodash';
 import { promises as fs } from 'fs';
+import debug from 'debug';
+
+const log = debug('page-loader:log');
+const error = debug('page-loader:error');
 
 const tagSrcMapping = {
   link: 'href',
@@ -24,8 +26,12 @@ const convertRelUrlToFileName = (relUrl) => {
   return `${fileName}${pathData.ext}`;
 };
 
-const saveFile = (source, dest) => axios.get(source, { responseType: 'arraybuffer' })
-  .then((response) => fs.writeFile(dest, response.data));
+const saveWebPageToFile = (source, dest) => {
+  log(`saving page ${source} to file ${dest}`);
+  return axios.get(source, { responseType: 'arraybuffer' })
+    .then((response) => fs.writeFile(dest, response.data))
+    .catch(error);
+};
 
 export default (sourceUrl, destDir = process.cwd()) => {
   const assetsDirName = `${convertUrlToFileName(sourceUrl)}_files`;
@@ -33,6 +39,9 @@ export default (sourceUrl, destDir = process.cwd()) => {
   const destFilepath = path.join(destDir, fileName);
   const destAssetsDirpath = path.join(destDir, assetsDirName);
   let $;
+
+  log(`saving ${sourceUrl} as ${fileName} in ${destFilepath}`);
+  log(`assets dir: ${destAssetsDirpath}`);
 
   return axios.get(sourceUrl)
     .then((response) => {
@@ -44,27 +53,39 @@ export default (sourceUrl, destDir = process.cwd()) => {
         const $el = cheerio(el);
         const urlAttrName = tagSrcMapping[el.tagName];
         const elSrc = $el.attr(urlAttrName);
+        log(`working on tag ${el.tagName} with ${urlAttrName}="${elSrc}"`);
 
         try {
           // eslint-disable-next-line no-new
           new URL(elSrc); // throw if url is relative
+          log('url is absolute, skip');
         } catch (e) {
+          log('url is relative, process');
           const elFilename = convertRelUrlToFileName(elSrc);
           const elFilepath = path.join(assetsDirName, elFilename);
+          log(`new rel url: ${elFilepath}`);
           $el.attr(urlAttrName, elFilepath);
-          const promise = saveFile(
-            url.resolve(sourceUrl, elSrc),
+          const absSrcUrl = new URL(elSrc, sourceUrl);
+          const promise = saveWebPageToFile(
+            absSrcUrl.href,
             path.join(destAssetsDirpath, elFilename),
           );
           promises.push(promise);
         }
       });
 
+      log(`${promises.length} assets total`);
       if (promises.length === 0) { // only make a dest dir for html file if no assets
-        return fs.mkdir(destDir, { recursive: true }).catch(_.noop);
+        return fs.mkdir(destDir, { recursive: true })
+          .then(() => log(`${destDir} dir created`))
+          .catch(error);
       }
 
       return fs.mkdir(destAssetsDirpath, { recursive: true })
+        .then(() => log(`${destAssetsDirpath} dir created`))
+        .catch(error)
         .then(() => Promise.all(promises));
-    }).then(() => fs.writeFile(destFilepath, $.html()));
+    }).then(() => fs.writeFile(destFilepath, $.html())
+      .then(() => log(`${destFilepath} written\nfinished\n---------------------------`))
+      .catch(error));
 };
