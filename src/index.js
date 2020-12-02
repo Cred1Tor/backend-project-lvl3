e7ab297d1e5959e3696d64172e29b5c60773a0d2
Aleksandr Saitgalin <cred1tor@yandex.ru>
2020-12-03 02:19:39 +0600

    move loadAssets() to utils.js, remove error handling, keep listr only for assets loading

--- src/index.js
+++ src/index.js
@@ -3,116 +3,28 @@ import path from 'path';
 import cheerio from 'cheerio';
 import { promises as fs } from 'fs';
 import debug from 'debug';
-import Listr from 'listr';
-import { saveWebPageToFile, convertUrlToFileNameWithExt, convertUrlToFileNameWithoutExt } from './utils.js';
+import { convertUrlToFileNameWithoutExt, loadAssets } from './utils.js';
 import 'axios-debug-log';
 
 const log = debug('page-loader');
 
-const tagSrcMapping = {
-  link: 'href',
-  script: 'src',
-  img: 'src',
-};
-
 export default (sourceUrl, destDir = process.cwd()) => {
-  const tasks = new Listr([
-    {
-      title: `Connecting to ${sourceUrl}`,
-      task: (ctx) => {
-        const baseName = convertUrlToFileNameWithoutExt(sourceUrl);
-        const fileName = `${baseName}.html`;
-        ctx.srcHostname = new URL(sourceUrl).hostname;
-        ctx.assetsDirName = `${baseName}_files`;
-        ctx.destFilepath = path.join(destDir, fileName);
-        ctx.destAssetsDirpath = path.join(destDir, ctx.assetsDirName);
+  const baseName = convertUrlToFileNameWithoutExt(sourceUrl);
+  const fileName = `${baseName}.html`;
+  const srcHostname = new URL(sourceUrl).hostname;
+  const assetsDirName = `${baseName}_files`;
+  const destFilepath = path.join(destDir, fileName);
+  const destAssetsDirpath = path.join(destDir, assetsDirName);
+  let $;
 
-        log(`saving ${sourceUrl} as ${ctx.destFilepath}`);
-        log(`assets dir: ${ctx.destAssetsDirpath}`);
+  log(`saving ${sourceUrl} as ${destFilepath}`);
+  log(`assets dir: ${destAssetsDirpath}`);
 
-        return fs.stat(destDir)
-          .then((stats) => {
-            if (!stats.isDirectory()) {
-              throw new Error(`ENOTDIR: ${destDir} is not a directory`, destDir);
-            }
-          })
-          .then(() => fs.access(ctx.destFilepath)
-            .catch(() => true)
-            .then((caught) => {
-              if (!caught) {
-                throw new Error(`${ctx.destFilepath} already exists`, ctx.destFilepath);
-              }
-            }))
-          .then(() => fs.access(ctx.destAssetsDirpath)
-            .catch(() => true)
-            .then((caught) => {
-              if (!caught) {
-                throw new Error(`${ctx.destAssetsDirpath} already exists`, ctx.destAssetsDirpath);
-              }
-            }))
-          .then(() => axios.get(sourceUrl)
-            .catch((e) => {
-              throw new Error(`${e.message} (${e.config.method} ${e.config.url})`);
-            }))
-          .then((response) => {
-            ctx.$ = cheerio.load(response.data, { decodeEntities: false });
-          });
-      },
-    },
-    {
-      title: 'Saving assets',
-      task: (ctx) => {
-        const elements = ctx.$('link[href], script[src], img[src]');
-        const assetTasks = new Listr([], { concurrent: true, exitOnError: false });
-
-        elements.each((_i, el) => {
-          const $el = cheerio(el);
-          const urlAttrName = tagSrcMapping[el.tagName];
-          const elSrc = $el.attr(urlAttrName);
-          log(`working on tag ${el.tagName} with ${urlAttrName}="${elSrc}"`);
-          const fullSrcUrl = new URL(elSrc, sourceUrl);
-
-          if (fullSrcUrl.hostname !== ctx.srcHostname) {
-            log('url is absolute, skip');
-          } else {
-            log('url is relative, process');
-            const elFilename = convertUrlToFileNameWithExt(fullSrcUrl.href);
-            const elFilepath = path.join(ctx.assetsDirName, elFilename);
-            log(`new rel url: ${elFilepath}`);
-            $el.attr(urlAttrName, elFilepath);
-            const promise = fs.mkdir(ctx.destAssetsDirpath)
-              .then(() => log(`${ctx.destAssetsDirpath} dir created`))
-              .catch((err) => {
-                if (err.code === 'EEXIST') {
-                  return;
-                }
-                throw err;
-              })
-              .then(() => {
-                const source = fullSrcUrl.href;
-                const dest = path.join(ctx.destAssetsDirpath, elFilename);
-                log(`saving page ${source} to file ${dest}`);
-                return saveWebPageToFile(source, dest);
-              });
-            assetTasks.add({
-              title: `Saving ${elSrc}`,
-              task: () => promise,
-            });
-          }
-        });
-
-        return assetTasks;
-      },
-    },
-    {
-      title: 'Saving main page',
-      task: (ctx) => fs.writeFile(ctx.destFilepath, ctx.$.html(), 'utf-8')
-        .then(() => log(`${ctx.destFilepath} written\nfinished\n---------------------------`)),
-    },
-  ]);
-
-  return tasks.run().catch((e) => {
-    log(e.message);
-    throw e;
-  });
+  return axios.get(sourceUrl)
+    .then((response) => {
+      $ = cheerio.load(response.data, { decodeEntities: false });
+      return fs.mkdir(destAssetsDirpath);
+    }).then(() => loadAssets($, sourceUrl, srcHostname, destAssetsDirpath, assetsDirName))
+    .then(() => fs.writeFile(destFilepath, $.html(), 'utf-8'))
+    .then(() => log(`${destFilepath} written\nfinished\n---------------------------`));
 };
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,17 @@
 import axios from 'axios';
 import path from 'path';
+import Listr from 'listr';
+import debug from 'debug';
+import cheerio from 'cheerio';
 import { promises as fs } from 'fs';
+
+const log = debug('page-loader');
+
+const tagSrcMapping = {
+  link: 'href',
+  script: 'src',
+  img: 'src',
+};
 
 export const convertUrlToFileNameWithoutExt = (sourceUrl) => {
   const { host, pathname } = new URL(sourceUrl);
@@ -22,3 +33,55 @@ export const saveWebPageToFile = (source, dest) => axios.get(source, { responseT
   .catch((e) => {
     throw new Error(`${e.message} (${e.config.method} ${e.config.url})`);
   });
+
+export const loadAssets = ($, sourceUrl, srcHostname, destAssetsDirpath, assetsDirName) => {
+  const tasks = new Listr([
+    {
+      title: 'Saving assets',
+      task: () => {
+        const elements = $('link[href], script[src], img[src]');
+        const assetTasks = new Listr([], { concurrent: true, exitOnError: false });
+
+        elements.each((_i, el) => {
+          const $el = cheerio(el);
+          const urlAttrName = tagSrcMapping[el.tagName];
+          const elSrc = $el.attr(urlAttrName);
+          log(`working on tag ${el.tagName} with ${urlAttrName}="${elSrc}"`);
+          const fullSrcUrl = new URL(elSrc, sourceUrl);
+
+          if (fullSrcUrl.hostname !== srcHostname) {
+            log('url is absolute, skip');
+          } else {
+            log('url is relative, process');
+            const elFilename = convertUrlToFileNameWithExt(fullSrcUrl.href);
+            const elFilepath = path.join(assetsDirName, elFilename);
+            log(`new rel url: ${elFilepath}`);
+            $el.attr(urlAttrName, elFilepath);
+            const promise = fs.mkdir(destAssetsDirpath)
+              .then(() => log(`${destAssetsDirpath} dir created`))
+              .catch((err) => {
+                if (err.code === 'EEXIST') {
+                  return;
+                }
+                throw err;
+              })
+              .then(() => {
+                const source = fullSrcUrl.href;
+                const dest = path.join(destAssetsDirpath, elFilename);
+                log(`saving page ${source} to file ${dest}`);
+                return saveWebPageToFile(source, dest);
+              });
+            assetTasks.add({
+              title: `Saving ${elSrc}`,
+              task: () => promise,
+            });
+          }
+        });
+
+        return assetTasks;
+      },
+    },
+  ]);
+
+  return tasks.run();
+};
